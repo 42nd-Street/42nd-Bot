fe75273af28c2e1df40f80a91591175a17e36cb0
Tomkettle1 <tomkettleone@gmail.com>
2021-07-01 00:04:06 +0100

    Add slash command handling (#2)
    * Simple working slash command (v untidy)
    
    * Tidied up code
    
    * further comment
    
    * Updated readme, cleaned some comments
    
    * fixed indentation
    
    * Tidying text
    
    * Only allow devs to register commands & write slash command calls to console
    
    * Apply suggestions from code review

--- README.md
+++ README.md
@@ -23,3 +23,35 @@ To start, run
 ```
 docker compose up --build --force-recreate
 ```
+
+## Adding a (Slash) Command
+
+To add a command you must register it as a slash command, and add handling for when the command is called.
+
+### Registering the Command
+
+Just add your commands name & description to this array in the registerSlashCommands() function, then type `~registerslashcommands` (all lower case) somewhere where the bot can see it. You should recive confimation.
+```typescript
+const data = [
+    {
+        name: 'ping',
+        description: 'Test command, replies with pong.',
+    },
+    {
+        name: 'name', // All lower case & no spaces
+        description: 'description',
+    },
+];
+```
+
+### Handling the command
+
+Put you're own if statement checking for the command being called here, where you can call either a simple line of code or preferably a function containing all your code.
+
+```typescript
+client.on('interaction', async interaction => {
+    if (!interaction.isCommand()) return;
+    if (interaction.commandName === 'ping') await interaction.reply('Pong!');
+    if (interaction.commandName === 'ping') await yourFunction(interaction); // You may not need to pass the interaction through, depending on what your command is doing
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,57 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
+const DevIDs = ["170960451704717312"/*dislocated*/, "428934780764160010"/*tomkettle*/]
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user ? client.user.tag : "null"}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
+client.on('message', async message => {
+    if (message.content === 'ping') { //Debug test code, see if its still breathing
         message.channel.send('pong');
+        console.log('pong');
+        return;
+    }
+    if (message.content === '~registerslashcommands') { //Register slash commands
+        if (!DevIDs.includes(message.author.id)) return; // Check if author is a bot dev
+        await registerSlashCommands(message);
+        return;
     }
 });
 
+client.on('interaction', async interaction => { // stolen from https://deploy-preview-638--discordjs-guide.netlify.app/interactions/replying-to-slash-commands.html
+	if (!interaction.isCommand()) return;
+
+    console.log("Command called: "+interaction.commandName);
+
+	if (interaction.commandName === 'ping') await interaction.reply('Pong!');
+    // Calls to other command functions go here. Unless your command is a very simple single line (such as with /ping), please call a seperate function
+});
+
 client.login(process.env.DISCORD_TOKEN);
+
+async function registerSlashCommands(message: Discord.Message){
+    // see https://deploy-preview-638--discordjs-guide.netlify.app/interactions/registering-slash-commands.html
+    
+    // IMPORTANT: Remember to give the bot perms to create slash commands w/ the oauth page (application.commands privileges)
+
+    await client.application?.fetch();
+    
+    // Please add your command name & description here. 
+    const data = [
+        {
+            name: 'ping',
+            description: 'Test command, replies with pong.',
+        } // ,
+        // {
+        //     name: '',
+        //     description: '',
+        // },
+    ];
+
+    const commands = await client.application?.commands.set(data);
+    console.log(commands);
+
+    message.reply("Added commands");
+}
