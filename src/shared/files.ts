8e924bb3267357793d79ec8272d19baa95093cdc
timmcgilly <timmcgilly@gmail.com>
2021-07-21 21:28:00 +0000

    Added files comments

--- src/shared/files.ts
+++ src/shared/files.ts
@@ -3,11 +3,16 @@ import path from 'path';
 
 export function GetFilesRec(dir: string): string[] {
     let files: string[] = [];
+
+    // Go through each file and dir
     fs.readdirSync(dir).forEach(file => {
         const Absolute = path.join(dir, file);
         const parentDir = path.basename(dir);
 
+        // Check if dir (recursive)
         if (fs.statSync(Absolute).isDirectory()) {
+
+            // Recursive call to function with dir and then append parentDir to make path complete from root
             GetFilesRec(Absolute).forEach(f => {
                 files.push(path.join(parentDir, f));
             });
