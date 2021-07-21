5344af67ac097ff0390f5372c03473f293db0dba
timmcgilly <timmcgilly@gmail.com>
2021-07-21 22:09:22 +0000

    Comment get_random

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,6 @@
-export function get_random<T> (list: T[]): T{
-    return list[Math.floor((Math.random()*list.length))];
-  }
+/**
+ * Gets random item from list
+**/
+export function get_random<T>(list: T[]): T {
+    return list[Math.floor((Math.random() * list.length))];
+}
