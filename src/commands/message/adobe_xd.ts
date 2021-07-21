add77f0e3a639cac8f784b3a922068174eddc82e
timmcgilly <timmcgilly@gmail.com>
2021-07-21 17:33:17 +0000

    Added Adobe Xd auto responder

---
+++ src/commands/message/adobe_xd.ts
@@ -0,0 +1,12 @@
+import { Message } from 'discord.js';
+import { msgEvent } from '../../shared/interfaces';
+import { AutoreplyEmbedGen } from '../../shared/embed';
+
+export function run(e: msgEvent) {
+    e.msg.channel.send(AutoreplyEmbedGen("Xd", "https://cdn.discordapp.com/attachments/405042469000970255/587229465478823955/220px-Adobe_XD_CC_icon.svg.png", e.msg))
+    e.msg.delete();
+}
+
+export function match(msg: Message) {
+    return msg.content === "Xd" || msg.content === "Adobe Xd";
+}
