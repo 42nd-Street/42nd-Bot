8b53a5839863ee77a265bd4ea8dafeabdd463863
timmcgilly <timmcgilly@gmail.com>
2021-07-21 17:33:00 +0000

    Removed unnecessary comment

--- src/commands/message/excuse_me.ts
+++ src/commands/message/excuse_me.ts
@@ -3,7 +3,6 @@ import { msgEvent } from '../../shared/interfaces';
 import { AutoreplyEmbedGen } from '../../shared/embed';
 
 export function run(e: msgEvent) {
-    // Note: As of writing (1/7/21) info on the new embed sytem is not properly documented
     e.msg.channel.send(AutoreplyEmbedGen("Excuse me", "https://cdn.discordapp.com/attachments/470215521141391360/516040429402324992/shirtimage-3.png", e.msg))
     e.msg.delete();
 }
