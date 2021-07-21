ab0fd21d968f71faffd283b7863d29b2fececf96
timmcgilly <timmcgilly@gmail.com>
2021-07-21 20:28:47 +0000

    Add dank

---
+++ src/commands/message/autoresponders/dank.ts
@@ -0,0 +1,13 @@
+import { Message } from 'discord.js';
+import { msgEvent } from '../../../shared/interfaces';
+import { AutoreplyEmbedGen } from '../../../shared/embed';
+
+export function run(e: msgEvent) {
+    e.msg.channel.send(AutoreplyEmbedGen("dank", "https://cdn.discordapp.com/attachments/405042469000970255/587229706835853312/220px-Adobe_XD_CC_icon.svg_-_Copy.png", e.msg))
+    e.msg.delete();
+}
+
+export function match(msg: Message) {
+    // Adds exception for Ryans id for danke
+    return msg.content.toLowerCase() === "dank" || (msg.content.toLowerCase() === "danke" && msg.author.id == "225723089810358282") ;
+}
