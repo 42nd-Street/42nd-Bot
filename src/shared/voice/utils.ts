3f0ff8f445c7a3095eca665006d20cee480d44e2
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2021-07-29 20:42:46 +0100

    chore: clean up voice utils

--- src/shared/voice/utils.ts
+++ src/shared/voice/utils.ts
@@ -6,46 +6,6 @@ import {
 } from '@discordjs/voice';
 import { createDiscordJSAdapter } from './adapter';
 
-export async function connectToChannel(channel: VoiceChannel) {
-	/*
-		Here, we try to establish a connection to a voice channel. If we're already connected
-		to this voice channel, @discordjs/voice will just return the existing connection for
-		us!
-	*/
-	const connection = joinVoiceChannel({
-		channelId: channel.id,
-		guildId: channel.guild.id,
-		adapterCreator: createDiscordJSAdapter(channel),
-	});
-
-	/*
-		If we're dealing with a connection that isn't yet Ready, we can set a reasonable
-		time limit before giving up. In this example, we give the voice connection 30 seconds
-		to enter the ready state before giving up.
-	*/
-	try {
-		/*
-			Allow ourselves 30 seconds to join the voice channel. If we do not join within then,
-			an error is thrown.
-		*/
-		await entersState(connection, VoiceConnectionStatus.Connecting, 3e3);
-		/*
-			At this point, the voice connection is ready within 30 seconds! This means we can
-			start playing audio in the voice channel. We return the connection so it can be
-			used by the caller.
-		*/
-		return connection;
-	} catch (error) {
-		/*
-			At this point, the voice connection has not entered the Ready state. We should make
-			sure to destroy it, and propagate the error by throwing it, so that the calling function
-			is aware that we failed to connect to the channel.
-		*/
-		connection.destroy();
-		throw error;
-	}
-}
-
 import https from 'https';
 import { PassThrough } from 'stream';
 import { cmdEvent } from '@shared/interactions/interfaces';
@@ -57,7 +17,6 @@ export async function getStream(url: string) {
 
 	https.get(url, (res) => res.pipe(stream))
 
-	//await new Promise(fullfill => stream.on('end',fullfill))
 	return stream;
 }
 
