277f006ea1fbdce5553f9d8331c0fecdb3848160
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2021-07-29 21:06:06 +0000

    fix: update imports for birb_police

--- src/commands/message/autoresponders/birb_police.ts
+++ src/commands/message/autoresponders/birb_police.ts
@@ -1,6 +1,6 @@
 import { Message } from 'discord.js';
-import { msgEvent } from '../../../shared/interfaces';
-import { AutoreplyEmbedGen } from '../../../shared/embed';
+import { msgEvent } from '@interactions/interfaces';
+import { AutoreplyEmbedGen } from '@shared/embed';
 
 export function run(e: msgEvent) {
     e.msg.channel.send(AutoreplyEmbedGen("The birb police", "https://cdn.discordapp.com/attachments/586659155138314261/620349820481175563/the_swarm_2.0.png", e.msg));
